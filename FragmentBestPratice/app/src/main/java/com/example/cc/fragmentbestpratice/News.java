f2de882a13ce80c04a08d621f77e631333c0e879
bert_cai <bert_cai@163.com>
2017-12-27 15:11:22 +0800

    learned the Broadcast

---
+++ FragmentBestPratice/app/src/main/java/com/example/cc/fragmentbestpratice/News.java
@@ -0,0 +1,26 @@
+package com.example.cc.fragmentbestpratice;
+
+/**
+ * Created by cc on 17-12-27.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
